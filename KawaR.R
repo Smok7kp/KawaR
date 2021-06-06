8efbcf6ae6189a2b805aa0d2657f3e50c68422db
Smok7kp <43822731+Smok7kp@users.noreply.github.com>
2021-06-06 17:15:23 +0200

    Import

---
+++ KawaR.R
@@ -0,0 +1,10 @@
+#Import Danych
+(e1<-read.csv("Exports.csv",sep=";"))
+e2 <- e1[,-1]
+rownames(e2) <- e1[,1]
+eksport <- as.data.frame(sapply(e2, as.numeric))
+(i1<-read.csv("Imports.csv",sep=";"))
+i2 <- i1[,-1]
+rownames(i2) <- i1[,1]
+import <- as.data.frame(sapply(i2, as.numeric))
+
