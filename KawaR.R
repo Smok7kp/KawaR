5c056a05300f4cabab949b1fc79ba09c095ccaad
Smok7kp <43822731+Smok7kp@users.noreply.github.com>
2021-06-06 17:58:59 +0200

    Pierwsza obróbka
    Serio nie chce mi się więcej robić przepraszam ide grać w skyrim

--- KawaR.R
+++ KawaR.R
@@ -7,4 +7,12 @@ eksport <- as.data.frame(sapply(e2, as.numeric))
 i2 <- i1[,-1]
 rownames(i2) <- i1[,1]
 import <- as.data.frame(sapply(i2, as.numeric))
-
+#Obróbka
+#wspó³cznik importu do eksportu
+wide<-vector()
+for (n in 1:30){
+(wide[n]<-(import[37,n]/eksport[56,n]))
+}
+eksport[56,]
+import[37,]
+wide
