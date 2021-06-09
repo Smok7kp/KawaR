8ad7adfee67b4ba66dc46cf9f352e2ada009bd3d
wiktorbarc <wiktorbarc21@gmail.com>
2021-06-09 15:22:04 +0200

    es
    xd

--- KawaR.R
+++ KawaR.R
@@ -1,18 +1,30 @@
 #Import Danych
-(e1<-read.csv("Exports.csv",sep=";"))
-e2 <- e1[,-1]
-rownames(e2) <- e1[,1]
-eksport <- as.data.frame(sapply(e2, as.numeric))
+getwd() #pobieramy lokalizacje plikow
+
+#operacja wczytywania danych eksportu
+(e1<-read.csv("Exports.csv",sep=";")) #wczytujemy plik do zmiennej pomocniczej
+e2 <- e1[,-1] #wczytujemy do drugiej zmiennej wszystkie rekordy z wyjatkiem pierwszej kolumny
+eksport <- as.data.frame(sapply(e2, as.numeric)) #zmieniamy typ danych ze znakowego na numeryczny
+(rownames(eksport) <- e1[,1]) #ustawiamy pierwsza kolumne jako nazwy wierszy
+(colnames(eksport) <- c(1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019)) #ustawiamy nazwy kolumn
+
+#operacja wczytywania danych importu (powtarzamy powyzsze czynnosci)
 (i1<-read.csv("Imports.csv",sep=";"))
 i2 <- i1[,-1]
-rownames(i2) <- i1[,1]
 import <- as.data.frame(sapply(i2, as.numeric))
-#Obróbka
-#wspó³cznik importu do eksportu
+(rownames(import) <- e1[,1])
+(colnames(import) <- c(1990,1991,1992,1993,1994,1995,1996,1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019))
+
+#Obrobka
+#wspolczynnik importu do eksportu
 wide<-vector()
 for (n in 1:30){
 (wide[n]<-(import[37,n]/eksport[56,n]))
 }
-eksport[56,]
-import[37,]
 wide
+(wide <- rbind(colnames(import), wide))
+
+
+
+
+
