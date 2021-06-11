7a7496fc426d700f8feee4f4d5a72e082d47b3ea
wiktorbarc <wiktorbarc21@gmail.com>
2021-06-11 23:16:10 +0200

    Update KawaR.R

--- KawaR.R
+++ KawaR.R
@@ -17,22 +17,59 @@ import <- as.data.frame(sapply(i2, as.numeric))
 
 #Obrobka
 #wspolczynnik importu do eksportu
-wide<-vector() #tworzymy pusty wektor zeby zapisac w nim wartosci
+w1<-vector() #tworzymy pusty wektor zeby zapisac w nim wartosci
 for (n in 1:30){ #jest 30 lat
-(wide[n]<-(import[37,n]/eksport[56,n])) #dla kazdego roku bierzemy sume import i dzielimy ja przez sume exportu, zapisujemy do wektora
+(w1[n]<-(import[37,n]/eksport[56,n])) #dla kazdego roku bierzemy sume import i dzielimy ja przez sume exportu, zapisujemy do wektora
 }
 
-(wide <- rbind(colnames(import), wide)) #dodajemy lata do wspolczynnika dla przejzystosci
-wide #wyswietlamy wspolczynnik
-plot(wide[1,], wide[2,], main="Wspolczynnik importu do eksportu", xlab="rok", ylab="wspolczynnik", type="h", col=1, xaxt="n")
-text(wide[2,], wide[1,], labels = wide[2,], pos=1)
-lines(wide[1,], wide[2,])
-points(wide[1,], wide[2,], pch=20)
-axis(1, at = seq(1990, 2019, by = 1), las=2)
+(wide <- rbind(as.numeric(colnames(import)), w1)) #dodajemy lata do wspolczynnika dla przejzystosci
+#wide #wyswietlamy wspolczynnik
+#plot(wide[1,], wide[2,], main="Wspolczynnik importu do eksportu", xlab="rok", ylab="wspolczynnik", type="h", col=1, xaxt="n")
+#text(wide[2,], wide[1,], labels = wide[2,], pos=1)
+#lines(wide[1,], wide[2,])
+#points(wide[1,], wide[2,], pch=20)
+#axis(1, at = seq(1990, 2019, by = 1), las=2)
+
+library(ggplot2)
+
+wide2<-data.frame(wide)
+
+import2 <- import[37,]
+eksport2 <- eksport[56,]
+dane<-data.frame(import2, eksport2)
+
+iks<-data.frame(wide[1,])
+
+ggplot(dane, aes(fill="dupa", y=seq(1990, 2019, by = 1), x=iks))+
+  geom_bar(position="dodge", stat="identity")
+
+
+
+?ggplot
+
+
+
+
+
+
+barplot(w1, main="Wspolczynnik importu do eksportu", xlab="rok", ylab="wspolczynnik", ylim=c(0,1.2),cex.names=0.8, names.arg=wide[1,])
+
+?barplot
+
+wide
+
+str(wide)
+
+str(w1)
+
+wide
+
+
+
 
 #min max
 
-which(import == min(import[37,]))
-which(import == max(import[37,]))
+#which(import == min(import[37,]))
+#which(import == max(import[37,]))
 
 
