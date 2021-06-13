9bb1fb346317dfe9f489d34eb7ee59580b3b534c
Smok7kp <43822731+Smok7kp@users.noreply.github.com>
2021-06-13 21:34:40 +0200

    :minidisc:

--- KawaR.R
+++ KawaR.R
@@ -23,91 +23,108 @@ for (n in 1:30){ #jest 30 lat
 }
 
 wide<- cbind(as.vector(t(as.numeric(colnames(import)))),w1) #dodajemy lata do wspolczynnika dla przejrzystosci
-wide
 
-#plot(wide[1,], wide[2,], main="Wspolczynnik importu do eksportu", xlab="rok", ylab="wspolczynnik", type="h", col=1, xaxt="n")
-#text(wide[2,], wide[1,], labels = wide[2,], pos=1)
-#lines(wide[1,], wide[2,])
-#points(wide[1,], wide[2,], pch=20)
-#axis(1, at = seq(1990, 2019, by = 1), las=2)
+#Wykres slopkowy z linia wyznacznika
 
-library(ggplot2)
-(r1 <- rep(1990:2019, times=2))
-(rok <- sort(r1))
-(legenda <- rep(c("import","eksport"), times=30))
-il1<-vector()
-for (n in 0:60){
+#przygotujemy ramkê dane pod wykres
+(r1 <- rep(1990:2019, times=2)) #pierwsza kolumna to ka¿da data powtorzona po sobie dwa razy
+(rok <- sort(r1)) #uzyskujemy to powtarzajac zestaw wszystkich lat dwa razy i sortujac
+(legenda <- rep(c("import","eksport"), times=30)) #podobnie druga kolumna to frazy import i eksport powtarzane w kolko
+il1<-vector() #trzecia kolumna to wartosci importu i eksportu dla kazdego roku
+for (n in 0:60){ #uzyskujemy ja za forem, ktory dla parzystych liczb wpisuje wartosc eksportu, a dla nieparzystych importu
   if(n%%2==0){
     il1[n]<- eksport[56,(n/2)]
   }
   if(n%%2==1){
-    il1[n]<- import[37,(as.integer(n/2)+1)]
+    il1[n]<- import[37,(as.integer(n/2)+1)]#jako ze przechodziny po forze z 1 do 60, to indeks bedziemy musieli podzielic przez 2, jako ze musimy to zrobic takze dla liczb nieparzystych, to wymyslilem taki trik z integerem, glupie rozwiazanie, ale dziala
   }
 }
 (ilosc<-unlist(il1))
-(w2 <- rep(w1, times=2))
-(wspolczynnik <- sort(w2)*50000)
+(w2 <- rep(w1, times=2))#czwarta kolumna to wartosc wspolczynnika obliczonego wczesniej, takze powielona 2 razy
+(wspolczynnik <- sort(w2)*50000) #mnozymy razy 50000, by wspolczynnik byl wyzej na grafie i roznice z roku na rok byly bardziej widoczne
 
-dane_graf_1 <- data.frame(rok,legenda,ilosc,wspolczynnik)
-dane_graf_1
+dane_graf_1 <- data.frame(rok,legenda,ilosc,wspolczynnik)#laczymy wszysto w jedna rakme
 
+library(ggplot2) #wczytujemy pakiet ggplot2
 ggplot(dane_graf_1) + 
-  geom_bar(aes(fill=legenda, y=ilosc, x=rok), position="dodge", stat="identity") +
+  geom_bar(aes(fill=legenda, y=ilosc, x=rok), position="dodge", stat="identity") + #tworzymy podwujny wykres kolumnowy
   expand_limits( x = c(1990,NA), y = c(0,NA)) +
   scale_x_continuous(breaks = scales::breaks_width(1)) +
   scale_y_continuous(breaks = scales::breaks_width(10000)) +
-  geom_line(aes(x=rok, y=wspolczynnik)) 
+  geom_line(aes(x=rok, y=wspolczynnik)) #i linie przedstawiajaca wspolczynnik
   
-
 #ggsave(file="wykres.eps",device="eps", path="/Users/wiktor/Desktop/KawaR", width=20, height=20, units="cm")
-
 
 #min max
 names(import)[which(as.vector(t(import[37,])) == min(as.vector(t(import[37,]))))] #min import
 names(import)[which(as.vector(t(import[37,])) == max(as.vector(t(import[37,]))))] #max import
 names(eksport)[which(as.vector(t(eksport[56,])) == min(as.vector(t(eksport[56,]))))] #min eksport
 names(eksport)[which(as.vector(t(eksport[56,])) == max(as.vector(t(eksport[56,]))))] #max eksport
+#lata w ktorych mamy ekstrema przydaja nam sie w dlaszej pracy
 
-max(as.vector(t(import[37,])))
-max(as.vector(t(eksport[56,])))
-
+#Mapa swiata z krajami importujacymi/eksportujacymi
 
 #install.packages("rgdal")
-library(rgdal)
-my_spdf <- readOGR( 
+library(rgdal) #wczytujemy, by moc wczytac plik z mapa
+kontor <- readOGR( #wczytujemy kontur mapy
   dsn= paste0(getwd(),"/TM_WORLD_BORDERS_SIMPL-0.3.shp") , 
   layer="TM_WORLD_BORDERS_SIMPL-0.3",
   verbose=FALSE
 )
+#Import
 
-Europa <- my_spdf[my_spdf@data$REGION==150 & my_spdf@data$NAME!="Russia" , ]
+#wybieramy tylko dane o Europie i resetujemy ich indeksy
+Europa <- kontor[kontor@data$REGION==150 & kontor@data$NAME!="Russia" , ]
 rownames(Europa@data) <- 1:nrow(Europa@data)
-
-Import_mapd <- Europa@data[Europa@data$NAME==gsub("   ", "", rownames(import)[2]),]
+#znajdujemy w danych mapy kraje dla ktorych my mamy dane o kawie
+Kraje_z_importem <- Europa@data[Europa@data$NAME==gsub("   ", "", rownames(import)[2]),]
 for (n in 3:37){
-  Import_mapd <- rbind(Import_mapd,Europa@data[Europa@data$NAME==gsub("   ", "", rownames(import)[n]),])
+  Kraje_z_importem <- rbind(Kraje_z_importem,Europa@data[Europa@data$NAME==gsub("   ", "", rownames(import)[n]),])
 }
+#sprawdzamy jakie indeksy maja te kraje, by tylko je pomalowac
+idimport<-as.numeric(rownames(Import_mapd))
 
-idi<-as.numeric(rownames(Import_mapd))
-idi
 
-# Palette of 30 colors
-library(RColorBrewer)
-kolorki <- brewer.pal(9, "YlOrRd") 
-kolorki <- colorRampPalette(kolorki)(100)
+library(RColorBrewer) #biblioteka do kolorow
+paleta <- brewer.pal(9, "YlOrRd") #wybralismy palte idaca od zoltego do czerwonego
+paleta <- colorRampPalette(paleta)(100) #robimy 100 krokow, bo zmiany sa male i przy malej ilosci krokow nie bylo by ich widac
 
-kraje<-c(2:3,5:6,9:29,31,33,35)
+kawa_kraje_i<-c(2:3,5:6,9:29,31,33,35) #indeksy tylko tych krajow, ktore maja zarowno dane importu i kontur (uzyskane manualnie patrzac na dane)
 
-for (i in 1:30){
-# Attribute the appropriate color to each country
-Barwa_od_Importu <- cut(import[kraje,i], 100)
-Kol_Mapa<-vector()
-Kol_Mapa[1:50] <- "#FFFFFF"
-Kol_Mapa[idi] <- kolorki[as.numeric(Barwa_od_Importu)]
-# Make the plot
-png(file=paste(i,"_mapa.png",sep=""))
-plot(Europa, col=Kol_Mapa ,  bg = "#A6CAE0", xlab=colnames(import)[i])
+for (i in 1:30){ #dla kazdego roku
+#towrzymy skale barw
+Barwa_od_Importu <- cut(import[kawa_kraje_i,i], 100)
+Kol_Import<-vector() #pusty wektor dla barw
+Kol_Import[1:50] <- "#FFFFFF" #wypelniamy wszytko na bialo
+Kol_Import[idimport] <- paleta[as.numeric(Barwa_od_Importu)] #po kolei do odpowiednich indeksow wpisujemy kolor zaleznie od ilosci improtu
+#rysowanie z zapisywanie
+png(file=paste("i_",i,"_mapa.png",sep="")) #nazwa z indeksem by nie nadpisywalo
+plot(Europa, col=Kol_Import ,  bg = "#A6CAE0", xlab=colnames(import)[i]) #bardzo prosta fukcja, przyjmuje mape konturowa europy, odpowiednio sperparowane kolory i kolor t³a, by moc je odruznic, podpisane takze rokiem by bylo widac zmiane gdy dane zmieniaj sie nieznacznie
 dev.off()
 }
+
+#Eksport (pomijamy komentarze, które by by³y takie same)
+
+Kraje_z_eksportem <- kontor@data[kontor@data$NAME==gsub("   ", "", rownames(eksport)[1]),]
+for (n in 2:54){
+  Kraje_z_eksportem <- rbind(Kraje_z_eksportem,kontor@data[kontor@data$NAME==gsub("   ", "", rownames(eksport)[n]),])
+}
+
+ideksport<-as.numeric(rownames(Kraje_z_eksportem))+1 #dodawanie jeden, by pokolorowac odpowiednie kraje
+
+kawa_kraje_e<-c(1,3:9,11,13:44,46:48,50:54)
+
+for (e in 1:30){
+  as.numeric(Barwa_od_Eksportu)
+Barwa_od_Eksportu <- cut(eksport[kawa_kraje_e,e], 100)
+Kol_Eksport<-vector()
+Kol_Eksport[1:246] <- "#FFFFFF" #mamy wiecej bo jest caly swiat
+Kol_Eksport[ideksport] <- paleta[as.numeric(Barwa_od_Eksportu)]
+Kol_Eksport
+png(file=paste("e_",e,"_mapa.png",sep=""))
+plot(kontor, col=Kol_Eksport ,  bg = "#A6CAE0", xlab=colnames(eksport)[e])
+dev.off()
+}
+
 
 
